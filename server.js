faa3d6f6df1246d7e5bbed8c9bc43aef4e5bafbe
tarun kaushik <86646398+ktarun1419@users.noreply.github.com>
2023-09-01 19:40:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/submit-form',async (req, res) => {
   // Create a Nodemailer transporter using your email service
   const transporter = await nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    secure:true ,
+    secure:false ,
     port: 587,
     auth: {
         user: 'mehakbrar811@gmail.com',
